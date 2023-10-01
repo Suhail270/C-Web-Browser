6c9760b6dc0c3e860c1ce30f99a9dd7a3473b349
Suhail <suhailz13ahmed@outlook.com>
2023-10-01 18:01:55 +0400

    optimized code to make it async wherever i can as it was lagging too much.
    ended up getting rid of some functionalities, need to redo them. currently working on displaying history in the UI.

--- WebBrowser/Browser.cs
+++ WebBrowser/Browser.cs
@@ -1,9 +1,9 @@
 ﻿using Gtk;
 using System;
-using System.Net;
-using System.Net.Http;
-using HtmlAgilityPack;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 public class WebBrowserApp : Window
 {
@@ -11,50 +11,44 @@ public class WebBrowserApp : Window
     private Button navigateButton;
     private Button homeButton;
     private TextView contentTextView;
-    private Label titleLabel; // Add a Label for displaying the title
+    private Label titleLabel;
     private ScrolledWindow scrolledWindow;
     private string currentUrl;
     private string homePath = "homeURL.txt";
     private string homePageUrl;
+    private string favouritesPath = "favourites.txt";
+    private string historyPath = "history.txt";
+    private bool startUp = true;
+    private Button historyButton; //TODO
 
-    private Dialog editHomeDialog;
-    private Entry editHomeEntry;
-    private Button editHomeButton;
-
-    
     public WebBrowserApp() : base("F20SC - CW1")
     {
         SetDefaultSize(800, 600);
         SetPosition(WindowPosition.Center);
 
-        // Create UI elements
         var mainVBox = new VBox();
         var mainHBox = new HBox();
-        var titleHBox = new HBox(); // Add a new HBox for title and reload button
+        var titleHBox = new HBox();
 
         addressEntry = new Entry();
         addressEntry.WidthChars = 50;
         navigateButton = new Button("Go");
         homeButton = new Button("Home");
-        editHomeButton = new Button("Edit Home");
         contentTextView = new TextView();
         scrolledWindow = new ScrolledWindow();
-        titleLabel = new Label(); // Initialize the Label
+        titleLabel = new Label();
 
         navigateButton.Clicked += NavigateButton_Clicked;
         homeButton.Clicked += HomeButton_Clicked;
-        editHomeButton.Clicked += EditHomeButton_Clicked;
 
         mainVBox.PackStart(mainHBox, false, false, 0);
-        mainVBox.PackStart(titleHBox, false, false, 0); // Add the titleHBox to mainVBox
+        mainVBox.PackStart(titleHBox, false, false, 0);
 
         mainHBox.PackStart(addressEntry, false, false, 0);
         mainHBox.PackStart(navigateButton, false, false, 10);
         mainHBox.PackStart(homeButton, false, false, 10);
-        mainHBox.PackStart(editHomeButton, false, false, 10); // Add the editHomeButton
 
-
-        titleHBox.PackStart(titleLabel, false, false, 10); // Add the titleLabel to titleHBox
+        titleHBox.PackStart(titleLabel, false, false, 10);
 
         var textBuffer = new TextBuffer(null);
         contentTextView.Buffer = textBuffer;
@@ -64,47 +58,6 @@ public class WebBrowserApp : Window
         scrolledWindow.Add(contentTextView);
         mainVBox.PackStart(scrolledWindow, true, true, 5);
 
-        // Inside the WebBrowserApp constructor
-        editHomeDialog = new Dialog("Edit Home Page URL", this, DialogFlags.Modal);
-
-        editHomeEntry = new Entry();
-        editHomeEntry.Text = homePageUrl; // Set the current home page URL as the default text
-        editHomeEntry.WidthChars = 50;
-
-        editHomeDialog.ContentArea.PackStart(editHomeEntry, true, true, 0);
-        editHomeDialog.AddButton("Cancel", ResponseType.Cancel);
-        editHomeDialog.AddButton("OK", ResponseType.Ok);
-
-        // Handle the response from the dialog when the "OK" button is clicked
-        editHomeDialog.Response += (o, args) =>
-        {
-            if (args.ResponseId == ResponseType.Ok)
-            {
-                // Update the homePageUrl with the edited URL
-                string url = editHomeEntry.Text;
-                if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
-                {
-                    ShowMessage("Invalid URL.");
-                    return;
-                }
-
-                else{
-                    homePageUrl = editHomeEntry.Text;
-
-                    // Save the updated home page URL to the "homeURL.txt" file
-                    File.WriteAllText(homePath, homePageUrl);
-
-                    editHomeDialog.Hide();
-
-                    ShowMessage("Home page updated successfully.");
-                    LoadHomePage();
-                }
-                
-            }
-            // Close the dialog
-            editHomeDialog.Hide();
-        };
-
         Add(mainVBox);
 
         DeleteEvent += (sender, args) => Application.Quit();
@@ -112,8 +65,9 @@ public class WebBrowserApp : Window
         contentTextView.Buffer.Text = "Enter a URL and click 'Go' to view HTML code.";
     }
 
-    // Define the ShowMessage method within the WebBrowserApp class
     private void ShowMessage(string message)
+{
+    GLib.Idle.Add(() =>
     {
         var messageDialog = new MessageDialog(
             this,
@@ -125,13 +79,13 @@ public class WebBrowserApp : Window
 
         messageDialog.Run();
         messageDialog.Destroy();
-    }
+        return false;
+    });
+}
 
-    private void EditHomeButton_Clicked(object sender, EventArgs e)
+    private void HomeButton_Clicked(object sender, EventArgs e)
     {
-        // Show the custom edit home dialog
-        editHomeDialog.ShowAll();
-
+        LoadHomePage();
     }
 
     private async void NavigateButton_Clicked(object sender, EventArgs e)
@@ -139,109 +93,127 @@ public class WebBrowserApp : Window
         string url = addressEntry.Text;
         if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
         {
-            contentTextView.Buffer.Text = "Invalid URL";
+            ShowMessage("Invalid URL.");
             return;
         }
 
         try
         {
+            string content = await FetchWebContentAsync(url);
+
+            // Display the HTML code in the TextView
+            contentTextView.Buffer.Text = content;
+            // Display the HTTP response status code
+            string statusCode = $"HTTP Status Code: {currentUrl}";
+            titleLabel.Text = statusCode;
+
+            // Parse the title of the web page and display it
+            string title = GetWebPageTitle(content);
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                titleLabel.Text += $" - Title: {title}";
+            }
+
+            // Store the current URL
+            currentUrl = url;
+        }
+        catch (Exception ex)
+        {
+            contentTextView.Buffer.Text = $"Error: {ex.Message}";
+            titleLabel.Text = string.Empty;
+        }
+    }
+
+    private async Task<string> FetchWebContentAsync(string url)
+    {
+        try
+        {
             using (HttpClient client = new HttpClient())
             {
                 HttpResponseMessage response = await client.GetAsync(url);
-                string content = await response.Content.ReadAsStringAsync();
 
                 if (response.IsSuccessStatusCode)
-                {
-                    // Display the HTML code in the TextView
-                    contentTextView.Buffer.Text = content;
-
-                    // Display the HTTP response status code
-                    string statusCode = $"HTTP Status Code: {(int)response.StatusCode} {response.ReasonPhrase}";
-                    titleLabel.Text = statusCode;
-
-                    // Parse the title of the web page and display it
-                    string title = GetWebPageTitle(content);
-                    if (!string.IsNullOrWhiteSpace(title))
-                    {
-                        titleLabel.Text += $" - Title: {title}";
-                    }
-
-                    // Store the current URL
+                {   
                     currentUrl = url;
+
+                    if(!startUp){
+                        await UpdateHistoryAsync();
+                    }
+                    else{
+                        startUp = false;
+                    }
+                    return await response.Content.ReadAsStringAsync();
                 }
                 else
                 {
-                    contentTextView.Buffer.Text = $"HTTP Error {response.StatusCode}: {content}";
-                    titleLabel.Text = string.Empty; // Clear the title if there's an error
+                    throw new HttpRequestException($"HTTP Error {(int)response.StatusCode}: {response.ReasonPhrase}");
                 }
             }
         }
         catch (HttpRequestException ex)
         {
-            contentTextView.Buffer.Text = $"Request Error: {ex.Message}";
-            titleLabel.Text = string.Empty; // Clear the title if there's an error
+            throw new Exception($"Request Error: {ex.Message}");
         }
     }
 
-    private void HomeButton_Clicked(object sender, EventArgs e)
-    {
-        LoadHomePage();
-    }
-    private void ReloadCurrentPage()
-    {
-        if (!string.IsNullOrWhiteSpace(currentUrl))
-        {
-            addressEntry.Text = currentUrl; // Set the address entry to the current URL
-            NavigateButton_Clicked(this, EventArgs.Empty); // Trigger a navigation to the current URL
-        }
-    }
-
-    // Helper method to load the home page URL
     private async void LoadHomePage()
     {
         try
         {
-            homePageUrl = File.ReadLines(homePath).FirstOrDefault();
+            homePageUrl = await ReadHomePageAsync();
 
-            using (HttpClient client = new HttpClient())
+            if (string.IsNullOrWhiteSpace(homePageUrl))
             {
-                HttpResponseMessage response = await client.GetAsync(homePageUrl);
-                string content = await response.Content.ReadAsStringAsync();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    // Display the HTML code in the TextView
-                    contentTextView.Buffer.Text = content;
-
-                    // Display the HTTP response status code
-                    string statusCode = $"HTTP Status Code: {(int)response.StatusCode} {response.ReasonPhrase}";
-                    titleLabel.Text = statusCode;
-
-                    // Parse the title of the web page and display it
-                    string title = GetWebPageTitle(content);
-                    if (!string.IsNullOrWhiteSpace(title))
-                    {
-                        titleLabel.Text += $" - Title: {title}";
-                    }
-
-                    // Store the current URL
-                    currentUrl = homePageUrl;
-                }
-                else
-                {
-                    contentTextView.Buffer.Text = $"HTTP Error {response.StatusCode}: {content}";
-                    titleLabel.Text = string.Empty; // Clear the title if there's an error
-                }
+                ShowMessage("Home page URL not set.");
+                return;
             }
+
+            string content = await FetchWebContentAsync(homePageUrl);
+
+            // Display the HTML code in the TextView
+            contentTextView.Buffer.Text = content;
+
+            // Display the HTTP response status code
+            string statusCode = $"HTTP Status Code: {currentUrl}";
+            titleLabel.Text = statusCode;
+
+            // Parse the title of the web page and display it
+            string title = GetWebPageTitle(content);
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                titleLabel.Text += $" - Title: {title}";
+            }
+
+            // Store the current URL
+            currentUrl = homePageUrl;
         }
-        catch (HttpRequestException ex)
+        catch (Exception ex)
         {
-            contentTextView.Buffer.Text = $"Request Error: {ex.Message}";
-            titleLabel.Text = string.Empty; // Clear the title if there's an error
+            contentTextView.Buffer.Text = $"Error: {ex.Message}";
+            titleLabel.Text = string.Empty;
         }
     }
 
-    // Helper method to parse the title of a web page
+    private async Task<string> ReadHomePageAsync()
+    {
+        if (File.Exists(homePath))
+        {
+            return await File.ReadAllTextAsync(homePath);
+        }
+
+        return string.Empty;
+    }
+
+    private async Task<string> UpdateHistoryAsync()
+    {
+        if (File.Exists(historyPath))
+        {
+            File.AppendAllText(historyPath, currentUrl+"\n");
+        }
+
+        return string.Empty;
+    }
+
     private string GetWebPageTitle(string htmlContent)
     {
         try
@@ -261,9 +233,7 @@ public class WebBrowserApp : Window
     {
         Application.Init();
         var app = new WebBrowserApp();
-        if (File.ReadLines(app.homePath).FirstOrDefault()!=null){
-            app.LoadHomePage();
-        }
+        app.LoadHomePage();
         app.ShowAll();
         Application.Run();
     }
